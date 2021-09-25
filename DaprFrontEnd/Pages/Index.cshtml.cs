3d76964e7d49975f7cdc50e2a616e482125fc9fd
Francis Chung <francis.c.chung@gmail.com>
2021-09-25 21:15:10 +1000

    Injecting DaprClient into IndexModel

--- DaprFrontEnd/Pages/Index.cshtml.cs
+++ DaprFrontEnd/Pages/Index.cshtml.cs
@@ -5,21 +5,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapr.Client;
+using System.Net.Http;
 
 namespace DaprFrontEnd.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly DaprClient _daprClient;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(DaprClient daprClient)
         {
-            _logger = logger;
+            _daprClient = daprClient ?? throw new ArgumentNullException(nameof(daprClient));
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
-
+            var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(
+                HttpMethod.Get, "daprbackend", "Weatherforecast"
+                );
+            ViewData["WeatherForecastData"] = forecasts;
         }
+
     }
 }
